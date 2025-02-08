15af082f43d5d933a6a9c53c99e1e11f0ce2f52f
jairjosafath <jairjosafath@gmail.com>
2025-02-08 13:39:57 +0100

    chores: cleaned up code

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,4 @@
-from flask import Flask, jsonify, request, json, Response, stream_with_context
-import requests
-from llm.chat import generate_response
-from requests.models import Response as Response_
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -19,20 +16,5 @@ def echo():
     return jsonify(data)
 
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    data = request.get_json()
-
-    def generate():
-        for chunk in generate_response(
-            user_input=data['query'],
-            model=data['model'],
-            num_predict=data['num_predict'],
-            disable_streaming=data['disable_streaming'],
-        ):
-            yield chunk.content
-    return stream_with_context(generate()), 200, {'Content-Type': 'text/plain'}
-
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
--- frontend/deepseek-rag/src/App.tsx
+++ frontend/deepseek-rag/src/App.tsx
@@ -1,11 +1,9 @@
 import { Chat } from "./components/Chat";
-import { FileUpload } from "./components/FileUpload";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center p-4 ">
-      <h1 className="text-2xl font-bold mb-4">Deepseeker</h1>
-      <FileUpload />
+      <h1 className="text-2xl font-bold mb-4">Deepseeker-01</h1>
       <Chat />
     </div>
   );
